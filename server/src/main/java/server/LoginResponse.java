2f92f48fcbd135b8f62f2a3a68cfd7d9e3d0f8b4
nameis_DarkRaven <emileeforbush@gmail.com>
2024-10-20 21:59:09 -0600

    Added spark, etc. packages. Added some classes from the tips slides.

---
+++ server/src/main/java/server/LoginResponse.java
@@ -0,0 +1,6 @@
+package server;
+
+public class LoginResponse extends Response {
+    String authtoken;
+    String username;
+}
---
+++ server/src/main/java/server/Server.java
@@ -0,0 +1,33 @@
+package server;
+
+import spark.*;
+
+public class Server {
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+
+        // Register your endpoints and handle exceptions here.
+        //Example code:
+//        Spark.post("/user", (req, res) ->
+//                (new RegisterHandler()).handleRequest(req,
+//                        res));
+
+        //This line initializes the server and can be removed once you have a functioning endpoint 
+        Spark.init();
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
+}
+
+
+
+
