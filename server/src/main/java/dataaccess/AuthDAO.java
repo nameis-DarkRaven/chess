59a01962260e0f3bfc2bec77ad70be527db5ed78
nameis_DarkRaven <emileeforbush@gmail.com>
2024-10-22 21:06:33 -0600

    Finished AuthDAO.java and MemoryAuthDAO.java (probably).

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,10 @@
 package dataaccess;
+import model.AuthData;
 
 public interface AuthDAO {
+    public String generateToken();
+    public void createAuth(AuthData auth);
+    public AuthData getAuth(String authToken) throws DataAccessException;
+    public void deleteAuth(String authToken) throws DataAccessException;
+    public void clear();
 }
---
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -0,0 +1,40 @@
+package dataaccess;
+
+import model.AuthData;
+
+import java.util.Collection;
+import java.util.UUID;
+
+public class MemoryAuthDAO implements AuthDAO {
+    private Collection<AuthData> auths;
+
+    public String generateToken() {
+        return UUID.randomUUID().toString();
+    }
+
+    @Override
+    public void createAuth(AuthData auth) {
+        auths.add(auth);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        for (AuthData auth : auths) {
+            if (auth.authToken() == authToken) {
+                return auth;
+            }
+        }
+        throw new DataAccessException("Invalid authorization.");
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        AuthData auth = getAuth(authToken);
+        auths.remove(auth);
+    }
+
+    @Override
+    public void clear() {
+        auths.clear();
+    }
+}
