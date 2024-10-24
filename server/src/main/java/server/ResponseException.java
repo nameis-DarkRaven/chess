9793ed908b72bdb07a324590095e2cd4f283263c
nameis_DarkRaven <emileeforbush@gmail.com>
2024-10-24 15:14:23 -0600

    Implemented my server. I am not sure what is wrong with it, if there is something wrong, but so far, it looks good. Added ErrorMessage.java to return error messages.

--- server/src/main/java/server/ResponseException.java
+++ server/src/main/java/server/ResponseException.java
@@ -4,4 +4,7 @@ public class ResponseException extends RuntimeException {
     public ResponseException(String message) {
         super(message);
     }
+    public int StatusCode(){
+        return 500;
+    }
 }
