5961efc57a45fa7d4e22359ee38a60480ccc52fe
nameis_DarkRaven <emileeforbush@gmail.com>
2024-11-01 16:24:30 -0600

    Maybe probably finished SQLUserDAO.java. I have a feeling I'll have to change a few things in the future, but this is a start.

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -1,4 +1,107 @@
 package dataaccess;
 
-public class SQLUserDAO {
+import com.google.gson.Gson;
+import model.UserData;
+
+import java.sql.*;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+public class SQLUserDAO implements UserDAO {
+
+    public SQLUserDAO() throws DataAccessException {
+        configureDatabase();
+    }
+
+    public void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var pStatement = conn.prepareStatement(statement)) {
+                    pStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", e.getMessage()));
+        }
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  users (
+              `user` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` varchar(256) NOT NULL,
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`user`),
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    private int executeUpdate(String statement, Object... objects) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < objects.length; i++) {
+                    var object = objects[i];
+                    if (object instanceof String p) ps.setString(i + 1, p);
+                    else if (object instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (object instanceof UserData p) ps.setString(i + 1, p.toString());
+                    else if (object == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
+
+
+    private UserData readUser(ResultSet resultSet)throws SQLException {
+        var json = resultSet.getString("json");
+        var user = new Gson().fromJson(json, UserData.class);
+        return user;
+    }
+
+
+    @Override
+    public UserData createUser(UserData user) throws DataAccessException {
+        var statement = "INSERT INTO users (user, password, email, json) VALUES (?, ?, ?, ?)";
+        var json = new Gson().toJson(user);
+        executeUpdate(statement, user.username(), user.password(), user.email(), json);
+        return new UserData(user.username(), user.password(), user.email());
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username, json FROM users WHERE username=?";
+            try (var pStatement = conn.prepareStatement(statement)) {
+//                pStatement.setString(1, id);
+                try (var resultSet = pStatement.executeQuery()) {
+                    if (resultSet.next()) {
+                        return readUser(resultSet);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
+        return null;
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        var statement = "TRUNCATE users";
+        executeUpdate(statement);
+    }
+
+
 }
