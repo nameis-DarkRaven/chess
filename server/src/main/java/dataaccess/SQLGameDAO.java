2cd140784ca3a43351ab84d217fa9f750f9b118f
nameis_DarkRaven <emileeforbush@gmail.com>
2024-11-01 16:54:47 -0600

    Implemented SQLGameDAO.java. There's probably something I need to fix.

--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -1,4 +1,142 @@
 package dataaccess;
 
-public class SQLGameDAO {
+import com.google.gson.Gson;
+import model.GameData;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.sql.*;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+public class SQLGameDAO implements GameDAO {
+
+    public SQLGameDAO() throws DataAccessException {
+        configureDatabase();
+    }
+
+    public void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var pStatement = conn.prepareStatement(statement)) {
+                    pStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format
+                    ("Unable to configure database: %s", e.getMessage()));
+        }
+    }
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  games (
+              `gameID` INT NOT NULL AUTO_INCREMENT,
+              `whiteUsername` varchar(256) DEFAULT NULL,
+              `blackUsername` varchar(256) DEFAULT NULL,
+              `gameName` varchar(256) NOT NULL,
+              `game` TEXT NOT NULL,
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`gameID`),
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    private int executeUpdate(String statement, Object... objects) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < objects.length; i++) {
+                    var object = objects[i];
+                    if (object instanceof String p) ps.setString(i + 1, p);
+                    else if (object instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (object instanceof GameData p) ps.setString(i + 1, p.toString());
+                    else if (object == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format
+                    ("Unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
+
+    private GameData readGame(ResultSet resultSet) throws SQLException {
+        var json = resultSet.getString("json");
+        return new Gson().fromJson(json, GameData.class);
+    }
+
+
+    @Override
+    public int createGame(String gameName) throws DataAccessException {
+        var statement = "INSERT INTO games (gameID, whiteUsername, blackUsername, " +
+                "gameName, game, json) VALUES (?, ?, ?, ?, ?, ?)";
+        var json = new Gson().toJson(gameName);
+        executeUpdate(statement, gameName, json);
+//        return new UserData(user.username(), user.password(), user.email());
+        return 0;
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT gameID, json FROM games WHERE gameID=?";
+            try (var pStatement = conn.prepareStatement(statement)) {
+                try (var resultSet = pStatement.executeQuery()) {
+                    if (resultSet.next()) {
+                        return readGame(resultSet);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format
+                    ("Unable to read data: %s", e.getMessage()));
+        }
+        return null;
+    }
+
+    @Override
+    public Collection<GameData> listGames(String authToken) throws DataAccessException {
+        var gameList = new ArrayList<GameData>();
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT gameID, json FROM games";
+            try (var ps = conn.prepareStatement(statement)) {
+                try (var rs = ps.executeQuery()) {
+                    while (rs.next()) {
+                        gameList.add(readGame(rs));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format
+                    ("Unable to read data: %s", e.getMessage()));
+        }
+        return gameList;
+    }
+
+    @Override
+    public void updateGame(int gameID, GameData game) throws DataAccessException {
+        var statement = "DELETE FROM games WHERE gameID=?";
+        executeUpdate(statement, gameID);
+
+        statement = "INSERT INTO games (gameID, whiteUsername, blackUsername, " +
+                "gameName, game, json) VALUES (?, ?, ?, ?, ?, ?)";
+        var json = new Gson().toJson(gameID);
+        executeUpdate(statement, gameID, game, json);
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+        var statement = "TRUNCATE games";
+        executeUpdate(statement);
+    }
+
+
 }
