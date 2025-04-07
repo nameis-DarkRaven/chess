475d373d586ecd334804319f041feb43d4989ff5
nameis_DarkRaven <emileeforbush@gmail.com>
2025-04-07 16:27:30 -0600

    Formatting, dependencies, and added move to MakeMoveCommand.

--- shared/src/main/java/websocket/commands/MakeMoveCommand.java
+++ shared/src/main/java/websocket/commands/MakeMoveCommand.java
@@ -1,11 +1,15 @@
 package websocket.commands;
 
+import chess.ChessMove;
+
 public class MakeMoveCommand extends UserGameCommand {
     private CommandType type;
     private String authToken;
     private Integer gameID;
+    private ChessMove move;
 
-    public MakeMoveCommand(CommandType commandType, String authToken, Integer gameID) {
+    public MakeMoveCommand(CommandType commandType, String authToken, Integer gameID, ChessMove move) {
         super(commandType, authToken, gameID);
+        this.move = move;
     }
 }
