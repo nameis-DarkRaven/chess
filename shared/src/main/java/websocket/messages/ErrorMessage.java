ef0d80d6c11f5bf23565e404a85fbfd289df4dca
nameis_DarkRaven <emileeforbush@gmail.com>
2025-04-10 16:02:34 -0600

    Added a getter for the error message.

--- shared/src/main/java/websocket/messages/ErrorMessage.java
+++ shared/src/main/java/websocket/messages/ErrorMessage.java
@@ -1,11 +1,14 @@
 package websocket.messages;
 
 public class ErrorMessage extends ServerMessage{
-    ServerMessage.ServerMessageType serverMessageType;
     String errorMessage;
 
     public ErrorMessage(String errorMessage) {
         super(ServerMessageType.ERROR);
         this.errorMessage = errorMessage;
+    }
+
+    public String message(){
+        return errorMessage;
     }
 }
