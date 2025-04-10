9133378ed6f0b0dc23475610e267485cff8e16ec
nameis_DarkRaven <emileeforbush@gmail.com>
2025-04-10 16:00:35 -0600

    Added sendError method to send errors to client.

--- server/src/main/java/server/websocket/Connection.java
+++ server/src/main/java/server/websocket/Connection.java
@@ -1,6 +1,9 @@
 package server.websocket;
 
+import com.google.gson.Gson;
+import org.eclipse.jetty.websocket.api.RemoteEndpoint;
 import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ErrorMessage;
 
 import java.io.IOException;
 
@@ -16,4 +19,10 @@ public class Connection {
     public void send(String msg) throws IOException {
         session.getRemote().sendString(msg);
     }
+
+    public void sendError(RemoteEndpoint remote, String message) throws IOException {
+        ErrorMessage error = new ErrorMessage(message);
+        remote.sendString(new Gson().toJson(error));
+    }
+
 }
